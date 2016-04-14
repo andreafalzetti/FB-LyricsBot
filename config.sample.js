524666437d1c2a98613773b1a076270e43342812
andreafalzetti <afalzetti@gmail.com>
2016-04-14 12:46:18 +0100

    oops

---
+++ config.sample.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.app = {};
+config.app.port = 8445;
+
+config.facebook = {};
+config.facebook.PAGE_ID = "";
+config.facebook.FB_VERIFY_TOKEN = "";
+config.facebook.PAGE_ACCESS_TOKEN = "";
+
+/* Do Not edit the following code */
+module.exports = config;
