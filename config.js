f73cc124856288d59fe21c7c9dd28f0804493d71
andreafalzetti <afalzetti@gmail.com>
2016-04-14 12:43:37 +0100

    Import

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.app = {};
+config.app.port = 8445;
+
+config.facebook = {};
+config.facebook.PAGE_ID = "782425181854134";
+config.facebook.FB_VERIFY_TOKEN = "andreya";
+config.facebook.PAGE_ACCESS_TOKEN = "CAADDFq9ePXQBAI6N2Ob88X9S23U3s5GXkRxuMbPsmIhkHQZCYEoSZCtwbV6RZBcLCujsxIplcH6K0Eh53RLzkxaD2UTN8TIqJLAM6IYKsrPl3Gt7jRgVZCD6CiaQM7hHIHm3mlnRDYMpFAyOLuhKQQG9rLwumQgBmt0pVYK082dqG0GhQrEISAfDODuRaePigNuWdZAsBlQZDZD";
+
+/* Do Not edit the following code */
+module.exports = config;
